42908add24de89e70d87c5956afafdc806d893fe
Jya0w0 <132446218+Jya0w0@users.noreply.github.com>
2024-03-27 21:26:46 +0900

    [Bronze V] Title: 시험 성적, Time: 64 ms, Memory: 5468 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.cs
@@ -0,0 +1,23 @@
+string s = Console.ReadLine();
+int score = int.Parse(s);
+
+if (score >= 90)
+{
+    Console.WriteLine("A");
+}
+else if (score >= 80)
+{
+    Console.WriteLine("B");
+}
+else if (score >= 70)
+{
+    Console.WriteLine("C");
+}
+else if (score >= 60)
+{
+    Console.WriteLine("D");
+}
+else
+{
+    Console.WriteLine("F");
+}
