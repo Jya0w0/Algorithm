de667ec15fa9604e92ca94f6af2cbe923c650261
Jya0w0 <132446218+Jya0w0@users.noreply.github.com>
2024-04-02 22:23:34 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 460 ms, Memory: 6580 KB -BaekjoonHub

--- 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.cs
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.cs
@@ -1,9 +1,11 @@
 int n = int.Parse(Console.ReadLine());
+char a = ' ';
+char b = '*';
 for(int j = 0; j < n; j++)
 {
     for (int i = 0; i < n; i++)
     {
-        Console.Write(i < n - j - 1 ? " " : "*");
+        Console.Write(i < n - j - 1 ? a : b);
     }
     Console.WriteLine();
 }
