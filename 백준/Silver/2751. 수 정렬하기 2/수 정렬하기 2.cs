fd7eae339bb263b4416f95bd2c142a24eef852d0
Jya0w0 <132446218+Jya0w0@users.noreply.github.com>
2024-04-09 21:09:28 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1700 ms, Memory: 50384 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Text;
+using System.Threading.Channels;
+
+namespace CodingTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] nums = new int[n];
+
+            for(int i = 0; i < n; i++)
+            {
+
+                nums[i] = int.Parse(Console.ReadLine());
+            }
+
+            Array.Sort(nums);
+
+            StringBuilder sb = new StringBuilder();
+
+            for(int j = 0; j < n; j++)
+            {
+                sb.Append(nums[j] + "\n");
+            }
+
+            Console.WriteLine(sb);
+        }
+    }
+}
