aaa471241462b6cfc37d8841e721a86fa9c1e18b
Jya0w0 <132446218+Jya0w0@users.noreply.github.com>
2024-04-25 21:33:17 +0900

    [Silver V] Title: 숫자 카드, Time: 684 ms, Memory: 85716 KB -BaekjoonHub

---
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Text;
+
+namespace CodingTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] a = new int[n];
+            string[] s = Console.ReadLine().Split();
+
+            for (int i = 0; i < n; i++)
+            {
+                a[i] = int.Parse(s[i]);
+            }
+            Array.Sort(a);
+
+            int m = int.Parse(Console.ReadLine());
+            int[] b = new int[m];
+            string[] ss = Console.ReadLine().Split();
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < m; i++)
+            {
+                int low = 0;
+                int high = n - 1;
+                b[i] = int.Parse(ss[i]);
+                bool isSame = false;
+
+                while(low <= high && !isSame)
+                {
+                    int mid = (low + high) / 2;
+                    if (b[i] == a[mid])
+                    {
+                        isSame = true;
+                    }
+                    else if (b[i] > a[mid])
+                    {
+                        low = mid + 1;
+                    }
+                    else
+                    {
+                        high = mid - 1;
+                    }
+                }
+                sb.Append(isSame ? "1\n" : "0\n");
+            }
+            Console.WriteLine(sb);
+        }
+    }
+}
