194ac104dc59638e68f21654bfca738a8c2ab473
Jya0w0 <132446218+Jya0w0@users.noreply.github.com>
2024-04-29 22:48:30 +0900

    [Silver IV] Title: 스택, Time: 80 ms, Memory: 7828 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.cs
+++ 백준/Silver/10828. 스택/스택.cs
@@ -8,34 +8,31 @@ namespace CodingTest
         static void Main(string[] args)
         {
             int n = int.Parse(Console.ReadLine());
-            string[] commands = new string[n];
             Stack<int> a = new Stack<int>();
             StringBuilder sb = new StringBuilder();
 
             for (int i = 0; i < n; i++)
             {
-                commands[i] = Console.ReadLine();
-                if (commands[i] == "pop")
+                string[] commands = Console.ReadLine().Split();
+                if (commands[0] == "push")
+                {
+                    a.Push(int.Parse(commands[1]));
+                }
+                else if (commands[0] == "pop")
                 {
                     sb.Append(a.Count == 0 ? "-1\n" : a.Pop() + "\n");
                 }
-                else if (commands[i] == "size")
+                else if (commands[0] == "size")
                 {
                     sb.Append(a.Count() + "\n");
                 }
-                else if (commands[i] == "empty")
+                else if (commands[0] == "empty")
                 {
                     sb.Append(a.Count == 0 ? "1\n" : "0\n");
                 }
-                else if (commands[i] == "top")
+                else if (commands[0] == "top")
                 {
                     sb.Append(a.Count == 0 ? "-1\n" : a.Peek() + "\n");
-                }
-                else
-                {
-                    string[] s = new string[2];
-                    s = commands[i].Split();
-                    a.Push(int.Parse(s[1]));
                 }
             }
             Console.WriteLine(sb);
