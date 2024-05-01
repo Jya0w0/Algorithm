34e7c28100b566ac2f1a5f187a28363b4b2b18c8
Jya0w0 <132446218+Jya0w0@users.noreply.github.com>
2024-05-01 22:33:40 +0900

    [level 2] Title: 피보나치 수, Time: 1.32 ms, Memory: 31.3 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.cs
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.cs
@@ -3,9 +3,9 @@ public class Solution {
         long answer = 0;
         long a = 0;
         long b = 1;
-        for(int i = 0; i <= n; i++) {
-            answer = a % 1234567;
-            a += b % 1234567;
+        for(int i = 2; i <= n; i++) {
+            answer = (a + b) % 1234567;
+            a = b;
             b = answer;
         }
         return (int)answer;
