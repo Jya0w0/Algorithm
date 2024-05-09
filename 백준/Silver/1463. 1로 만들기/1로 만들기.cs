1ce02b2c027554ea0a49d72815d24564bf227d14
Jya0w0 <132446218+Jya0w0@users.noreply.github.com>
2024-05-09 23:39:27 +0900

    [Silver III] Title: 1로 만들기, Time: 68 ms, Memory: 9380 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.cs
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cs
@@ -1,18 +1,11 @@
-int x = int.Parse(Console.ReadLine());
-int[] dp = new int[x + 1];
+int n = int.Parse(Console.ReadLine());
+int[] dp = new int[n + 1];
+dp[1] = 0;
 
-for (int i = 2; i <= x; i++)
+for (int i = 2; i <= n; i++)
 {
     dp[i] = dp[i - 1] + 1;
-    if (i % 3 == 0)
-    {
-        dp[i] = Math.Min(dp[i], dp[i / 3] + 1);
-    }
-    if (i % 2 == 0)
-    {
-        dp[i] = Math.Min(dp[i], dp[i / 2] + 1);
-    }
-
+    if (i % 3 == 0) dp[i] = Math.Min(dp[i / 3] + 1, dp[i]);
+    if (i % 2 == 0) dp[i] = Math.Min(dp[i / 2] + 1, dp[i]);
 }
-
-Console.WriteLine(dp[x]);
+Console.WriteLine(dp[n]);
