131c7deab614ccde12967aaa473a5960de31b5cc
Jya0w0 <132446218+Jya0w0@users.noreply.github.com>
2024-09-04 21:43:33 +0900

    [level 0] Title: 더 크게 합치기, Time: 0.03 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int a, int b) {
+    int answer = 0;
+    string str1 = to_string(a) + to_string(b);
+    string str2 = to_string(b) + to_string(a);
+    if (str1 >= str2)
+        answer = stoi(str1);
+    else
+        answer = stoi(str2);
+    return answer;
+}
