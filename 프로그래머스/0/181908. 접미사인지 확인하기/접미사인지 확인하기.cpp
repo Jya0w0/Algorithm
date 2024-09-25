f92adbd1d119781639266854ce9e58f7f213f52e
Jya0w0 <132446218+Jya0w0@users.noreply.github.com>
2024-09-25 22:08:56 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.02 ms, Memory: 4.02 MB -BaekjoonHub

--- 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.cpp
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.cpp
@@ -5,15 +5,10 @@ using namespace std;
 
 int solution(string my_string, string is_suffix) {
     int answer = 0;
-    vector<string> str;
     for (int i = 0; i < my_string.size(); i++)
     {
-        str.push_back(my_string.substr(i));
-    }
-
-    for (auto s : str)
-    {
-        if (s == is_suffix) answer = 1;
+        string str = my_string.substr(i);
+        if (str == is_suffix) answer = 1;
     }
     return answer;
 }
