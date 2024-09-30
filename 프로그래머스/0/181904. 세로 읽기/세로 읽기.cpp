442a765652c0701e12f44e2d387ddac2f6f19b6f
Jya0w0 <132446218+Jya0w0@users.noreply.github.com>
2024-09-30 21:40:25 +0900

    [level 0] Title: 세로 읽기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/0/181904. 세로 읽기/세로 읽기.cpp
+++ 프로그래머스/0/181904. 세로 읽기/세로 읽기.cpp
@@ -5,14 +5,9 @@ using namespace std;
 
 string solution(string my_string, int m, int c) {
     string answer = "";
-    int num = (my_string.size() + m - 1) / m;
-    for (int i = 0; i < num; i++) 
+    for (int i = c - 1; i < my_string.size(); i += m)
     {
-        int index = i * m + (c - 1);
-        if (index < my_string.size()) 
-        {
-            answer.push_back(my_string[index]);
-        }
+        answer.push_back(my_string[i]);
     }
     return answer;
 }
