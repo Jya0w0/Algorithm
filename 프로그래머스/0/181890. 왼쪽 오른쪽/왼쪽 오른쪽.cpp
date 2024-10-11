c68542c8a6a6965b17974a63c1a0ae1677f0f3a3
Jya0w0 <132446218+Jya0w0@users.noreply.github.com>
2024-10-11 22:03:53 +0900

    [level 0] Title: 왼쪽 오른쪽, Time: 0.03 ms, Memory: 4.16 MB -BaekjoonHub

--- 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.cpp
+++ 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.cpp
@@ -7,22 +7,27 @@ vector<string> solution(vector<string> str_list) {
     vector<string> answer;
     int a = -1;
     int b = -1;
-    for (int i = 0; i < str_list.size(); i++)
+    for (int i = 0; i < str_list.size(); i++) 
     {
-        if (str_list[i] == "l" && a == -1) a = i;
-        else if (str_list[i] == "r" && b == -1) b = i;
-
-        if (a != -1 && b != -1) break;
+        if (str_list[i] == "l" && a == -1) 
+        {
+            a = i;
+            break;
+        }
+        if (str_list[i] == "r" && b == -1) {
+            b = i;
+            break;
+        }
     }
 
-    if (a != -1 && (b == -1 || a < b))
+    if (a != -1) 
     {
         for (int i = 0; i < a; i++) 
         {
             answer.push_back(str_list[i]);
         }
     }
-    else if (b != -1)
+    else if (b != -1) 
     {
         for (int i = b + 1; i < str_list.size(); i++) 
         {
