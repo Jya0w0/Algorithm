c6ce9a03f75fce91825aa2dc0e5e0487bfd64b44
Jya0w0 <132446218+Jya0w0@users.noreply.github.com>
2024-10-11 21:48:47 +0900

    [level 0] Title: 왼쪽 오른쪽, Time: 0.02 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.cpp
@@ -0,0 +1,33 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<string> solution(vector<string> str_list) {
+    vector<string> answer;
+    int a = -1;
+    int b = -1;
+    for (int i = 0; i < str_list.size(); i++)
+    {
+        if (str_list[i] == "l" && a == -1) a = i;
+        else if (str_list[i] == "r" && b == -1) b = i;
+
+        if (a != -1 && b != -1) break;
+    }
+
+    if (a != -1 && (b == -1 || a < b))
+    {
+        for (int i = 0; i < a; i++) 
+        {
+            answer.push_back(str_list[i]);
+        }
+    }
+    else if (b != -1)
+    {
+        for (int i = b + 1; i < str_list.size(); i++) 
+        {
+            answer.push_back(str_list[i]);
+        }
+    }
+    return answer;
+}
