df8d7825b1b656a3f58358e4b30eaff126fce5f1
Jya0w0 <132446218+Jya0w0@users.noreply.github.com>
2024-12-02 19:40:05 +0900

    [Silver III] Title: N과 M (4), Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int n, m;
+vector<int> arr;
+
+void dfs(int index, int start) {
+    if (index == m)
+    {
+        for (int i = 0; i < m; i++)
+        {
+            cout << arr[i] << ' ';
+        }
+        cout << '\n';
+        return;
+    }
+
+    for (int i = start; i <= n; i++)
+    {
+        arr[index] = i;
+        dfs(index + 1, i);
+    }
+}
+
+int main() {
+    cin >> n >> m;
+
+    arr.resize(m);
+
+    dfs(0, 1);
+
+    return 0;
+}
