8c71f635ff247628cd59127eb90af41bf16d9ac8
Jya0w0 <132446218+Jya0w0@users.noreply.github.com>
2024-12-13 19:54:17 +0900

    [Silver IV] Title: 비트가 넘쳐흘러, Time: 36 ms, Memory: 4916 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/17419. 비트가 넘쳐흘러/비트가 넘쳐흘러.cc
+++ 백준/Silver/17419. 비트가 넘쳐흘러/비트가 넘쳐흘러.cc
@@ -1,26 +1,15 @@
 #include <iostream>
-#include <vector>
+#include <string>
+#include <algorithm>
 using namespace std;
 
 int main() {
     int n;
-    cin >> n;
+    string k;
+    cin >> n >> k;
 
-    int k = 0;
-    string str = "";
-    cin >> str;
-    for (char c : str)
-    {
-        k = (c - '0') | (k << 1);
-    }
-
-    int count = 0;
-    while (k != 0)
-    {
-        k = k - (k & ((~k) + 1));
-        count++;
-    }
-    cout << count;
+    int answer = count(k.begin(), k.end(), '1');
+    cout << answer;
 
     return 0;
 }
