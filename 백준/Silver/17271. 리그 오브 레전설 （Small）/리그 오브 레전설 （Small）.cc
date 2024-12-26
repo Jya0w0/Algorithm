2d044ddc3c653f7a38fdc37be102830bb726ec5b
Jya0w0 <132446218+Jya0w0@users.noreply.github.com>
2024-12-26 19:34:55 +0900

    [Silver II] Title: 리그 오브 레전설 (Small), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/17271. 리그 오브 레전설 （Small）/리그 오브 레전설 （Small）.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    vector<int> dp(n + 1);
+    dp[0] = 1;
+
+    for (int i = 1; i <= n; i++) 
+    {
+        dp[i] = dp[i - 1];
+
+        if (i >= m) 
+        {
+            dp[i] = (dp[i] + dp[i - m]) % 1000000007;
+        }
+    }
+
+    cout << dp[n] << endl;
+
+    return 0;
+}
